ec83430df0e4286d9372fa13aaa70d57ada04eaa
rlima <rlima@atlassian.com>
2021-09-22 23:46:38 +1000

    Binary search

--- src/main/kotlin/com/example/challenges/searching/easy-1-BinarySearch.kt
+++ src/main/kotlin/com/example/challenges/searching/easy-1-BinarySearch.kt
@@ -7,14 +7,31 @@ package com.example.challenges.searching
 fun main() {
     val array = listOf(0, 1, 21, 33, 45, 45, 61, 71, 72, 73) //sorted array
     val target = 33
-    val expected = 3 //index of 33
+    val expected = 3 //index of the target 33
 
     val result = binarySearch(array, target)
-    println(result == expected)
+//    println(result == expected)
     println(result)
 }
 
 // O(log(n)) time | O(1) space
 fun binarySearch(array: List<Int>, target: Int): Int {
+    var left = 0
+    var right =  array.size - 1
+
+    while (left <= right) {
+        val middle = (left + right) / 2
+
+        val match = array[middle]
+
+        if (match == target) {
+            return middle
+        } else if (target < match) {
+            right = middle -1
+        } else {
+            left = middle + 1
+        }
+    }
+
     return -1
 }
